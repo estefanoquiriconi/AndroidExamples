1b43425fa4b1812690ec9981e7835129088b2b68
estefanoquiriconi <estefanoquiriconi@gmail.com>
2024-10-10 11:46:05 -0300

    Intent example

--- Intent/app/src/main/java/com/example/intent/MainActivity.java
+++ Intent/app/src/main/java/com/example/intent/MainActivity.java
@@ -1,5 +1,6 @@
-package com.example.binding;
+package com.example.intent;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
@@ -7,29 +8,30 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
-import com.example.binding.databinding.ActivityMainBinding;
+
+import com.example.intent.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
-
 
     ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
 
         binding = ActivityMainBinding.inflate(this.getLayoutInflater());
         setContentView(binding.getRoot());
 
-        EdgeToEdge.enable(this);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
 
-        binding.btnChangeText.setOnClickListener(view -> {
-            binding.textView.setText("Hello Android!");
+        binding.btnGo.setOnClickListener(view -> {
+            Intent intent = new Intent(this, SecondActivity.class);
+            startActivity(intent);
         });
     }
 }
--- Intent/app/src/main/java/com/example/intent/SecondActivity.java
+++ Intent/app/src/main/java/com/example/intent/SecondActivity.java
@@ -1,4 +1,4 @@
-package com.example.binding;
+package com.example.intent;
 
 import android.os.Bundle;
 
@@ -7,29 +7,29 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
-import com.example.binding.databinding.ActivityMainBinding;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.intent.databinding.ActivitySecondBinding;
 
+public class SecondActivity extends AppCompatActivity {
 
-    ActivityMainBinding binding;
+    ActivitySecondBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
 
-        binding = ActivityMainBinding.inflate(this.getLayoutInflater());
+        binding = ActivitySecondBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        EdgeToEdge.enable(this);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
 
-        binding.btnChangeText.setOnClickListener(view -> {
-            binding.textView.setText("Hello Android!");
+        binding.btnReturn.setOnClickListener(v -> {
+            finish();
         });
     }
 }
