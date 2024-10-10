a7e541d300bc0fbae358b2047c2d36f6bdb9e32c
estefanoquiriconi <estefanoquiriconi@gmail.com>
2024-10-10 11:30:27 -0300

    viewBinding example

--- Binding/app/src/main/java/com/example/binding/MainActivity.java
+++ Binding/app/src/main/java/com/example/binding/MainActivity.java
@@ -7,18 +7,29 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import com.example.binding.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        binding = ActivityMainBinding.inflate(this.getLayoutInflater());
+        setContentView(binding.getRoot());
+
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        binding.btnChangeBackground.setOnClickListener(view -> {
+            binding.textView.setText("Hello Android!");
         });
     }
 }
